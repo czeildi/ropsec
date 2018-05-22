3e565c4056a5a73993d91ab0a82ed3d704a357b5
Ildiko Czeller <czeildi@gmail.com>
2018-05-22 16:20:09 -0700

    fix R CMD check

--- R/audit_results.R
+++ R/audit_results.R
@@ -5,7 +5,7 @@
 #' @return testthat report
 #' @export
 #'
-#' @examples \dontrun(summarize_system_check())
+#' @examples \dontrun{summarize_system_checks()}
 summarize_system_checks <- function() {
   system.file("tests", "simple", "macos-simple-test.R", package = "ropsec", mustWork = TRUE) %>%
     testthat::test_file()
