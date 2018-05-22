4b4238746f27dd348c419c6d7d5eb72b37c1f469
Ildiko Czeller <czeildi@gmail.com>
2018-05-22 16:14:54 -0700

    add function to run testthat style tests

---
+++ R/audit_results.R
@@ -0,0 +1,12 @@
+#' testthat style system checks
+#'
+#' Test system settings like ssh key, password requirement etc.
+#'
+#' @return testthat report
+#' @export
+#'
+#' @examples \dontrun(summarize_system_check())
+summarize_system_checks <- function() {
+  system.file("tests", "simple", "macos-simple-test.R", package = "ropsec", mustWork = TRUE) %>%
+    testthat::test_file()
+}
