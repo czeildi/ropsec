4b20f7a3149fec167c1e05ef6273b94fb547c45a
Ildiko Czeller <czeildi@gmail.com>
2019-07-04 21:43:09 +0200

    attempt to use correct home directory on windows

--- inst/tests/simple/windows-simple-test.R
+++ inst/tests/simple/windows-simple-test.R
@@ -1,18 +1,18 @@
 # sw_vers -productVersion
 context("SSH Configuration - existence")
 test_that("ssh directory exists", {
-  expect_true(file.exists(path.expand("~/.ssh")))
+  expect_true(file.exists(fs::path_expand("~/.ssh")))
 })
 
 context("SSH Configuration - keys")
 test_that("ssh has public/private keys", {
-  expect_true(file.exists(path.expand("~/.ssh/id_rsa")))
-  expect_true(file.exists(path.expand("~/.ssh/id_rsa.pub")))
+  expect_true(file.exists(fs::path_expand("~/.ssh/id_rsa")))
+  expect_true(file.exists(fs::path_expand("~/.ssh/id_rsa.pub")))
 })
 
 context("SSH Configuration - key size")
 test_that("ssh key size is appropriate", {
-  if (file.exists(path.expand("~/.ssh/id_rsa.pub"))) {
+  if (file.exists(fs::path_expand("~/.ssh/id_rsa.pub"))) {
     x <- openssl::read_pubkey("~/.ssh/id_rsa.pub")
     expect_true(x$size >= 2048, label = sprintf("Key size of [%s] is below recommended value", x$size))
   }
@@ -37,15 +37,15 @@ test_that("gpg is configured", {
 
 context("~/.Rprofile permissions are sane (if ~/.Rprofile exists)")
 test_that("~/.Rprofile permissions are sane", {
-  if (file.exists(path.expand("~/.Rprofile"))) {
-    expect_true(file.info(path.expand("~/.Rprofile"))$mode == structure(416L, class = "octmode"))
+  if (file.exists(fs::path_expand("~/.Rprofile"))) {
+    expect_true(file.info(fs::path_expand("~/.Rprofile"))$mode == structure(416L, class = "octmode"))
   }
 })
 
 context("~/.Renviron permissions are sane (if ~/.Renviron exists)")
 test_that("~/.Renviron permissions are sane", {
-  if (file.exists(path.expand("~/.Renviron"))) {
-    expect_true(file.info(path.expand("~/.Renviron"))$mode == structure(416L, class = "octmode"))
+  if (file.exists(fs::path_expand("~/.Renviron"))) {
+    expect_true(file.info(fs::path_expand("~/.Renviron"))$mode == structure(416L, class = "octmode"))
   }
 })
 
